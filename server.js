a08e3582aaa4b9f221a424ea8257d9e0a7615d78
Tony Chiang <chiangloveslynn@gmail.com>
2017-01-06 00:16:30 +0800

    production test

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const app = require('express')()
 const Nuxt = require('nuxt')
 
-const nuxt = new Nuxt()
-nuxt.build()
-.then(() => {
-  // Render every route with Nuxt.js
+// We instantiate Nuxt.js with the options
+const isProd = process.env.NODE_ENV === 'production'
+let config = require('./nuxt.config.js')
+config.dev = !isProd
+const nuxt = new Nuxt(config)
+// No build in production
+const promise = (isProd ? Promise.resolve() : nuxt.build())
+promise.then(() => {
   app.use(nuxt.render)
-  // Server listening
   app.listen(9487)
+  console.log('Server is listening on http://localhost:9487')  // eslint-disable-line no-console
+})
+.catch((error) => {
+  console.error(error)  // eslint-disable-line no-console
+  process.exit(1)
 })
