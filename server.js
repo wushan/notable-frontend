09167404ad01474d05b79aed3562359e37dad83d
wushan <chiangloveslynn@gmail.com>
2017-05-31 22:58:51 +0800

    -

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-const app = require('express')()
 const Nuxt = require('nuxt')
+const app = require('express')()
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
 
-// We instantiate Nuxt.js with the options
-const isProd = process.env.NODE_ENV === 'production'
+app.set('port', port)
+// Import API Routes
+// app.use('/api', require('./api/index'))
+
+// Import and Set Nuxt.js options
 let config = require('./nuxt.config.js')
-config.dev = !isProd
+config.dev = !(process.env.NODE_ENV === 'production')
+
+// Init Nuxt.js
 const nuxt = new Nuxt(config)
-// No build in production
-const promise = (isProd ? Promise.resolve() : nuxt.build())
-promise.then(() => {
-  app.use(nuxt.render)
-  app.listen(5000)
-  console.log('Server is listening on http://localhost:9487')  // eslint-disable-line no-console
-})
-.catch((error) => {
-  console.error(error)  // eslint-disable-line no-console
-  process.exit(1)
-})
+app.use(nuxt.render)
+
+// Build only in dev mode
+if (config.dev) {
+  nuxt.build()
+  .catch((error) => {
+    console.error(error) // eslint-disable-line no-console
+    process.exit(1)
+  })
+}
+
+// Listen the server
+app.listen(port, host)
+console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
